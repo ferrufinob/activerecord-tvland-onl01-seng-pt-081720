76f059404953bb9239175ad4dbdac0651e46c8a0
Brenda Ferrufino <ferrufinob@gmail.com>
2020-10-29 21:27:02 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+    belongs_to :network
+    has_many :characters
+
+    def actors_list
+        self.characters.collect do |t|
+            "#{t.actor.first_name} #{t.actor.last_name}"
+        end
+        
+    end
   
 end
---
+++ db/migrate/007_add_genre_column.rb
@@ -0,0 +1,8 @@
+class AddGenreColumn < ActiveRecord::Migration[5.2]
+
+    def change
+        add_column :shows, :genre, :string
+    end
+
+
+end
